b6df3f21ffc539283721f1798c0c358f8fcd1b81
Paul Unsted <ed__84@hotmail.com>
2020-02-12 17:08:12 -0500

    Added alerts for not giving timer a name, and incorrect datetime entry.

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,7 @@ document.addEventListener(
     // If the clicked element doesn't have the right selector, bail
     if (!event.target.matches("#click-me")) return;
 
-    // Don't follow the link
+    // Don't let button trigger default action
     event.preventDefault();
 
     // get name and date from form
@@ -25,30 +25,36 @@ document.addEventListener(
     // find difference between dates in milliseconds
     var diff = setDate - currMil;
 
-    console.log(diff);
-
-    var seconds = Math.floor((diff / 1000) % 60),
-      minutes = Math.floor((diff / (1000 * 60)) % 60),
-      hours = Math.floor((diff / (1000 * 60 * 60)) % 24),
-      days = Math.floor(diff / (1000 * 60 * 60 * 24));
-
-    hours = hours < 10 ? "0" + hours : hours;
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-
-    var placeholder = "";
-
-    if (days > 1) {
-      placeholder = " days ";
+    if (isNaN(diff) === true) {
+      alert("Please enter a date, time, and AM/PM.");
+    } else if (n === "") {
+      alert("Please enter a timer name.");
     } else {
-      placeholder = " day ";
+      // format time as day(s), hh:mm:ss
+      var seconds = Math.floor((diff / 1000) % 60),
+        minutes = Math.floor((diff / (1000 * 60)) % 60),
+        hours = Math.floor((diff / (1000 * 60 * 60)) % 24),
+        days = Math.floor(diff / (1000 * 60 * 60 * 24));
+
+      hours = hours < 10 ? "0" + hours : hours;
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+      var placeholder = "";
+
+      if (days > 1) {
+        placeholder = " days ";
+      } else {
+        placeholder = " day ";
+      }
+
+      let formatted =
+        days + placeholder + hours + ":" + minutes + ":" + seconds;
+
+      name.innerHTML = "<h1>" + n + "</h1>";
+
+      counter.innerHTML = "<h2> Time remaining: " + formatted + " </h2>";
     }
-
-    let formatted = days + placeholder + hours + ":" + minutes + ":" + seconds;
-
-    name.innerHTML = "<h2>" + n + "</h2>";
-
-    counter.innerHTML = "<h2> Time remaining: " + formatted + " </h2>";
   },
   false
 );
